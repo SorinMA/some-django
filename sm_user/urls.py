79ac0b9a405e182c107bb08b5962961769dd2006
Patriciu <sorinmartinescu9dev@gmail.com>
2019-06-26 13:51:03 +0300

    Little changes and begining of the Login Page

--- sm_user/urls.py
+++ sm_user/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import url
 from django.contrib import admin
-
+from django.contrib.auth.views import LoginView, LogoutView
 
 from .views import home_page
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'home$', home_page, name="home_page")
+    url(r'home$', home_page, name="home_page"),
+    url(r'login$', LoginView.as_view(template_name='sm_user/login_form.html'), name='sm_login'),
+    url(r'logout$', LogoutView.as_view(), name='sm_logout')
 ]
