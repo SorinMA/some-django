df14ba2ac3e02d778b774e8ca22b3cdc89f92e03
Patriciu <sorinmartinescu9dev@gmail.com>
2019-06-26 12:36:27 +0300

    Static content and home_page template work

---
+++ sm_user/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import url
+from django.contrib import admin
+
+from .views import home_page
+
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+    url(r'home$', home_page)
+]
